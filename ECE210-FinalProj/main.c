f4618a87349d5f85e320d2e183297685a9cf8341
Hax0rDoge <hax0rdoge@Alpine.local>
2017-12-04 17:37:08 -0600

    Initial files

---
+++ ECE210-FinalProj/main.c
@@ -0,0 +1,19 @@
+//*****************************************************************************
+// main.c
+// Author: Prayag Bhakar
+//*****************************************************************************
+
+// Turn Debuggin off
+#undef DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x) printf x
+#else
+#define DEBUG_PRINT(x) do {} while (0)
+#endif
+
+int main(void){
+	
+}
+
+
